5626bb1f93f63bc6362e7eadacdf80861a288a18
Dylan Walters <dylan@lawn-10-91-108-173.lawn.gatech.edu>
2023-10-17 16:04:07 -0400

    Initial Login Screen. Not hooked up to Auth

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import PersonCard from './PersonCard'; // Import the PersonCard component
+import LoginScreen from './loginScreen'; // Import the LoginScreen
 
 const App = () => {
   const [data, setData] = useState([]);
-
+  const [username, setUsername] = useState(null);  // New state to track login
+  
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/advisor/getall')
-      .then((response) => response.json())
-      .then((jsonData) => setData(jsonData))
-      .catch((error) => console.error(error));
-  }, []);
-
+    if (username) {  // Only fetch data if logged in
+      fetch('http://127.0.0.1:5000/advisor/getall')
+        .then((response) => response.json())
+        .then((jsonData) => setData(jsonData))
+        .catch((error) => console.error(error));
+    }
+  }, [username]);
+  
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        {}
-        {data.map((username, index) => (
-          <PersonCard key={index} username={username} />
-        ))}
-      </ScrollView>
+      {username ? (
+        <ScrollView>
+          {data.map((username, index) => (
+            <PersonCard key={index} username={username} />
+          ))}
+        </ScrollView>
+      ) : (
+        <LoginScreen onLogin={setUsername} />
+      )}
     </SafeAreaView>
   );
 };
