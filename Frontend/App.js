50730e45494fbbb1f4098f50bc98ececa2c4fc4e
Dylan Walters <dylan@lawn-10-91-108-173.lawn.gatech.edu>
2023-10-18 14:11:34 -0400

    Added a registration screen. Connected login and registration to api. Further bug fixes / updates likely required.

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import PersonCard from './PersonCard'; // Import the PersonCard component
-import LoginScreen from './loginScreen'; // Import the LoginScreen
+import LoginScreen from './Screens/loginScreen'; // Import the LoginScreen
+import RegistrationScreen from './Screens/registrationScreen';
 
 const App = () => {
   const [data, setData] = useState([]);
   const [username, setUsername] = useState(null);  // New state to track login
+  const [isOnLoginScreen, setIsOnLoginScreen] = useState(true); // Use state to track screen, need to update later
   
   useEffect(() => {
     if (username) {  // Only fetch data if logged in
@@ -15,6 +17,14 @@ const App = () => {
         .catch((error) => console.error(error));
     }
   }, [username]);
+
+  const handleGoToRegister = () => {
+    setIsOnLoginScreen(false);
+  };
+  
+  const handleBackToLogin = () => {
+    setIsOnLoginScreen(true);
+  };
   
   return (
     <SafeAreaView style={styles.container}>
@@ -24,9 +34,11 @@ const App = () => {
             <PersonCard key={index} username={username} />
           ))}
         </ScrollView>
+      ) : isOnLoginScreen ? (
+        <LoginScreen onLogin={setUsername} onGoToRegister={handleGoToRegister} />
       ) : (
-        <LoginScreen onLogin={setUsername} />
-      )}
+      <RegistrationScreen onRegister={handleBackToLogin} onGoBack={handleBackToLogin} />
+    )}
     </SafeAreaView>
   );
 };
